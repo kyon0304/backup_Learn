226949eff70a24c4a230b399f779fa29dc0d253a
kyon <kyon0304@gmail.com>
2012-08-30 20:30:01 +0800

    Commit - @ 2012年 08月 30日 星期四 20:30:01 CST

---
+++ python/grabWeb.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# file: grabWeb.py
+# date: 2012-8-30
+
+from urllib import urlretrieve
+
+def firstNonBlank(lines):
+    for eachLine in lines:
+        if not eachLine.strip():
+            continue
+        else:
+            return eachLine
+    
+def firstLast(webpage):
+    f = open(webpage)
+    lines = f.readlines()
+    f.close()
+    print firstNonBlank(lines)
+    lines.reverse()
+    print firstNonBlank(linexs)
+
+def download(url = 'http://www', process = firstLast):
+    try:
+        retval = urlretrieve(url)[0]
+    except IOError:
+        retval = None
+    if retval:
+        process(retval)
+
+if __name__ == '__main__':
+    download()
