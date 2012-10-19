af530fbcde261eb857b9559b714130dbca42b29f
kyon <kyon0304@gmail.com>
2012-10-19 20:30:01 +0800

    Commit - @ 2012年 10月 19日 星期五 20:30:01 CST

--- python/challenge/21.py
+++ python/challenge/21.py
@@ -3,8 +3,25 @@
 # file: 21.py offline readme.txt&package.pack
 # date: 2012-10-16 author: kyon
 
-import zlib
+import zlib, bz2
 
-data = open('package.pack').read()
-dc = zlib.decompress(data)
-print `dc[-1]`, `data[0]`
+st = open('package.pack').read()
+#st = zlib.decompress(st)
+#print `st[-1]`, `st[:4]`
+log = ''
+log_len = len(log)
+while True:
+    try:
+        st = zlib.decompress(st)
+        log += ' '
+    except:
+        try:
+            st = bz2.decompress(st)
+            log += '#'
+        except:
+            if log_len == len(log): break
+            st = st[::-1]
+            print log[log_len:]
+            log_len = len(log)
+
+open('21_package.unpack','wb').write(st)
